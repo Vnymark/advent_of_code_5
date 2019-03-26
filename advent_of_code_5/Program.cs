cdad752d42ef1c7266a8284f67a9d40a4992266a
Viktor Nymark <vnymark@gmail.com>
2019-03-26 19:37:20 +0100

    Working solution to part 1

--- advent_of_code_5/Program.cs
+++ advent_of_code_5/Program.cs
@@ -7,9 +7,10 @@ namespace advent_of_code_5
 {
     class Program
     {
-        public static List<Unit> UnitList;
         static void Main(string[] args)
         {
+
+            //Add data
             var inputPath = @"../../../input.txt";
             string inputString = File.ReadAllText(inputPath);
             char[] inputArray = inputString.ToCharArray();
@@ -24,6 +25,60 @@ namespace advent_of_code_5
                 };
                 UnitList.Add(unit);
             }
+
+            //Calculate Polymer
+            List<Unit> ValidUnits = new List<Unit>();
+            Unit uBefore = new Unit();
+            int i = 0;
+            bool invalid = false;
+            bool keepGoing = true;
+
+            UpdateValidUnits();
+            while (keepGoing)
+            {
+                i = 0;
+                invalid = false;
+                Console.WriteLine(ValidUnits.Count());
+                foreach (Unit u in ValidUnits)
+                {
+                    
+                    if (i > 0)
+                    {
+                        if (u.Char != uBefore.Char && char.ToUpperInvariant(u.Char) == char.ToUpperInvariant(uBefore.Char))
+                        {
+                            uBefore.Valid = false;
+                            u.Valid = false;
+                            invalid = true;
+                            break;
+                        }
+                    }
+                    uBefore = u;
+                    i++;
+                }
+                if(invalid)
+                {
+                    UpdateValidUnits();
+                }
+                else
+                {
+                    keepGoing = false;
+                }
+            }
+
+            //Function to update which units still exist in the Polymer
+            void UpdateValidUnits()
+            {
+                ValidUnits = new List<Unit>();
+                foreach (Unit u in UnitList)
+                {
+                    if (u.Valid)
+                    {
+                        ValidUnits.Add(u);
+                    }
+                }
+            }
+
+
             Console.ReadKey();
         }
 
