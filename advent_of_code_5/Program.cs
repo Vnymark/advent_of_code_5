53d38fd1381714d8cc6a3d11f315d5824f01fcab
Viktor Nymark <vnymark@gmail.com>
2019-03-26 15:14:40 +0100

    Added one input "cleansing".

--- advent_of_code_5/Program.cs
+++ advent_of_code_5/Program.cs
@@ -11,7 +11,28 @@ namespace advent_of_code_5
         {
             var inputPath = @"../../../input.txt";
             string inputString = File.ReadAllText(inputPath);
-            char[] inputArray = inputString.ToCharArray(); ;
+            char[] inputArray = inputString.ToCharArray();
+
+            int i = 0;
+            int j = i;
+            List<char> charList = new List<char>();
+            foreach (char c in inputArray)
+            {
+                
+                if (i != 0)
+                {
+                    j = i - 1;
+                }
+                if (char.ToUpperInvariant(c) != char.ToUpperInvariant(inputArray[j]))
+                {
+                    charList.Add(inputArray[i]);
+                }
+                i++;
+            }
+            Console.ReadKey();
         }
+
     }
+
+    
 }
