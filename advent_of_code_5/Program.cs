a7419622856dc7066ed2b6f8623fca116dbbaa6f
Viktor Nymark <vnymark@gmail.com>
2019-03-26 22:25:01 +0100

    Working Solution to part 2. Switched to just removing Units from ValidUnits, in CalculatePolymer(), since recreating ValidUnits everytime a new Unit became invalid was a huge time sink.
    On Part 2, it works as a solution because it only updates ValidUnits once, before the CalculatePolymer() is run.
    (Tried using lists only, but there was basically no difference in execution time).

--- advent_of_code_5/Program.cs
+++ advent_of_code_5/Program.cs
@@ -14,8 +14,8 @@ namespace advent_of_code_5
             var inputPath = @"../../../input.txt";
             string inputString = File.ReadAllText(inputPath);
             char[] inputArray = inputString.ToCharArray();
-            
             List<Unit> UnitList = new List<Unit>();
+
             foreach (char c in inputArray)
             {
                 Unit unit = new Unit
@@ -28,41 +28,45 @@ namespace advent_of_code_5
 
             //Calculate Polymer
             List<Unit> ValidUnits = new List<Unit>();
-            Unit uBefore = new Unit();
-            int i = 0;
-            bool invalid = false;
-            bool keepGoing = true;
-
             UpdateValidUnits();
-            while (keepGoing)
+            Console.WriteLine(CalculatePolymer()); 
+            
+            int CalculatePolymer()
             {
-                i = 0;
-                invalid = false;
-                Console.WriteLine(ValidUnits.Count());
-                foreach (Unit u in ValidUnits)
+                Unit uNow = new Unit();
+                Unit uBefore = new Unit();
+                int i = 0;
+                bool invalid = false;
+                bool keepGoing = true;
+                while (keepGoing)
                 {
-                    
-                    if (i > 0)
+                    i = 0;
+                    invalid = false;
+                    foreach (Unit u in ValidUnits)
                     {
-                        if (u.Char != uBefore.Char && char.ToUpperInvariant(u.Char) == char.ToUpperInvariant(uBefore.Char))
+                        if (i > 0)
                         {
-                            uBefore.Valid = false;
-                            u.Valid = false;
-                            invalid = true;
-                            break;
+                            if (u.Char != uBefore.Char && char.ToUpperInvariant(u.Char) == char.ToUpperInvariant(uBefore.Char))
+                            {
+                                invalid = true;
+                                uNow = u;
+                                break;
+                            }
                         }
+                        uBefore = u;
+                        i++;
                     }
-                    uBefore = u;
-                    i++;
+                    if (invalid)
+                    {
+                        ValidUnits.Remove(uBefore);
+                        ValidUnits.Remove(uNow);
+                    }
+                    else
+                    {
+                        keepGoing = false;
+                    }
                 }
-                if(invalid)
-                {
-                    UpdateValidUnits();
-                }
-                else
-                {
-                    keepGoing = false;
-                }
+                return ValidUnits.Count();
             }
 
             //Function to update which units still exist in the Polymer
@@ -78,7 +82,39 @@ namespace advent_of_code_5
                 }
             }
 
+            //Part 2
+            char[] distinctArray = inputArray.Distinct().ToArray();
+            string[] stringArray = distinctArray.Select(x => x.ToString()).ToArray();
+            string[] distinctStringArray = stringArray.Distinct(StringComparer.OrdinalIgnoreCase).ToArray();
+            int latestPolymerCount = 0;
+            int lowestPolymerCount = 0;
+            string lowestString = null;
+            foreach (string s in distinctStringArray)
+            {
+                Console.WriteLine("Calculating Polymer without the unit type: {0}", s);
+                char c = char.Parse(s);
+                foreach (Unit u in UnitList)
+                {
+                    if(char.ToUpper(u.Char) == char.ToUpper(c))
+                    {
+                        u.Valid = false;
+                    }
+                    else
+                    {
+                        u.Valid = true;
+                    }
+                }
+                UpdateValidUnits();
+                latestPolymerCount = CalculatePolymer();
+                if (lowestPolymerCount == 0 || lowestPolymerCount > latestPolymerCount)
+                {
+                    lowestPolymerCount = latestPolymerCount;
+                    lowestString = s;
+                }
+                
 
+            }
+            Console.WriteLine("Polymer count: {0} Achived by removing unit type: {1}", lowestPolymerCount, lowestString);
             Console.ReadKey();
         }
 
